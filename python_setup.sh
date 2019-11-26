370bb6f738bc4180ce92edbf85d72f43f38da524
lcl-airflow <herman.cheung@loblaw.com>
2019-11-26 10:23:58 -0500

    startup scripts

---
+++ python_setup.sh
@@ -0,0 +1,11 @@
+PROJECT=$(gcloud config list project --format "value(core.project)")
+BUCKET=gs://$PROJECT-dataflow
+gsutil mkdir $BUCKET
+gsutil cp data/export* $BUCKET/model/
+gzip -kdf data/images.txt.gz
+gsutil cp data/images.txt $BUCKET/input/
+
+git clone https://github.com/GoogleCloudPlatform/dataflow-prediction-example
+python -m virtualenv airflow-dataflow
+source airflow-dataflow/bin/activate
+pip install apache-beam[gcp]
