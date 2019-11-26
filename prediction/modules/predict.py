f6c32751cde413456a547a5ac95eecec51adb31a
lcl-airflow <herman.cheung@loblaw.com>
2019-11-26 11:44:19 -0500

    Tue Nov 26 15:36:42 UTC 2019

--- prediction/modules/predict.py
+++ prediction/modules/predict.py
@@ -19,68 +19,32 @@ import logging
 import os
 
 import apache_beam as beam
-import tensorflow as tf
+from apache_beam.options.pipeline_options import PipelineOptions
 
+class MyOptions(PipelineOptions):
 
-def singleton(cls):
-  instances = {}
-  def getinstance(*args, **kwargs):
-    if cls not in instances:
-      instances[cls] = cls(*args, **kwargs)
-    return instances[cls]
-  return getinstance
-
-
-@singleton
-class Model():
-
-  def __init__(self, checkpoint):
-    with tf.Graph().as_default() as graph:
-      sess = tf.InteractiveSession()
-      saver = tf.train.import_meta_graph(os.path.join(checkpoint, 'export.meta'))
-      saver.restore(sess, os.path.join(checkpoint, 'export'))
-
-      inputs = json.loads(tf.get_collection('inputs')[0])
-      outputs = json.loads(tf.get_collection('outputs')[0])
-
-      self.x = graph.get_tensor_by_name(inputs['image'])
-      self.p = graph.get_tensor_by_name(outputs['scores'])
-      self.input_key = graph.get_tensor_by_name(inputs['key'])
-      self.output_key = graph.get_tensor_by_name(outputs['key'])
-      self.sess = sess
-
-
-class PredictDoFn(beam.DoFn):
-
-  def process(self, element, checkpoint):
-    model = Model(checkpoint)
-    input_key = int(element['key'])
-    image = element['image'].split(',')
-    output_key, pred = model.sess.run(
-        [model.output_key, model.p],
-        feed_dict={model.input_key: [input_key], model.x: [image]})
-    result = {}
-    result['key'] = output_key[0]
-    for i, val in enumerate(pred[0].tolist()):
-      result['pred%d' % i] = val
-    return [result]
+  @classmethod
+  def _add_argparse_args(cls, parser):
+    parser.add_argument('--input', dest='input', required=True,
+                      help='Input file to process.')
+    parser.add_argument('--output', dest='output', required=True,
+                      help='Output file to write results to.')
+    parser.add_argument('--model', dest='model', required=True,
+                      help='Checkpoint file of the model.')
+    parser.add_argument('--source', dest='source', required=True,
+                      help='Data source location (cs|bq).')
 
 
 def run(argv=None):
   parser = argparse.ArgumentParser()
-  parser.add_argument('--input', dest='input', required=True,
-                      help='Input file to process.')
-  parser.add_argument('--output', dest='output', required=True,
-                      help='Output file to write results to.')
-  parser.add_argument('--model', dest='model', required=True,
-                      help='Checkpoint file of the model.')
-  parser.add_argument('--source', dest='source', required=True,
-                      help='Data source location (cs|bq).')
-  known_args, pipeline_args = parser.parse_known_args(argv)
+  
 
-  print (known_args.input)
-  print (known_args.output)
-  print (known_args.model)
-  print (known_args.source)
+
+p = beam.Pipeline(options=MyOptions())
+
+print (p.options.input)
+print (p.options.output)
+print (p.options.model)
+print (p.options.source)
 
 
